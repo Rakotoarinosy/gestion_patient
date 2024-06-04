8f128a73ab6b36de8061ddd0c63f7bcac12db220
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-06-04 11:03:07 +0300

    change routes to lazy loading

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router';
-import { PatientListComponent } from './components/patient/patient-list/patient-list.component';
-import { PatientEditComponent } from './components/patient/patient-edit/patient-edit.component';
-import { PatientCreateComponent } from './components/patient/patient-create/patient-create.component';
-import { MedecinCreateComponent } from './components/medecin/medecin-create/medecin-create.component';
-import { MedecinListComponent } from './components/medecin/medecin-list/medecin-list.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'create-patient', component: PatientCreateComponent },
-  { path: 'patients', component: PatientListComponent },
-  { path: 'patient/edit/:id', component: PatientEditComponent }, // Route for editing a patient
-  { path: 'create-medecin', component: MedecinCreateComponent },
-  { path: 'medecins', component: MedecinListComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'create-patient', loadComponent: () => import('./components/patient/patient-create/patient-create.component').then(m => m.PatientCreateComponent) },
+  { path: 'patients', loadComponent: () => import('./components/patient/patient-list/patient-list.component').then(m => m.PatientListComponent) },
+  { path: 'patient/edit/:id', loadComponent: () => import('./components/patient/patient-edit/patient-edit.component').then(m => m.PatientEditComponent) }, // Route for editing a patient
+  { path: 'create-medecin', loadComponent: () => import('./components/medecin/medecin-create/medecin-create.component').then(m => m.MedecinCreateComponent) },
+  { path: 'medecins', loadComponent: () => import('./components/medecin/medecin-list/medecin-list.component').then(m => m.MedecinListComponent) },
+  { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
   // { path: 'medecin/edit/:id', component: MedecinEditComponent },
   { path: '', redirectTo: '/medecins', pathMatch: 'full' }
 ];
--- front-end/src/app/components/medecin/medecin-edit/medecin-edit.component.ts
+++ front-end/src/app/components/medecin/medecin-edit/medecin-edit.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Medecin } from '../../../Entity/medecin';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MedecinService } from '../../../services/medecin.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-medecin-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './medecin-edit.component.html',
   styleUrl: './medecin-edit.component.scss'
 })
-export class MedecinEditComponent {
+export class MedecinEditComponent implements OnInit {
+  id: number = 0;
+  medecin: Medecin = new Medecin();
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private medecinService: MedecinService
+  ) { }
+
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id')!;
+    this.medecinService.getMedecin(this.id).subscribe(response => {
+      this.medecin = response.data;
+    });
+  }
+
+
+  saveMedecin() {
+    this.medecinService.createMedicin(this.medecin).subscribe(() => {
+      this.router.navigate(['/medecins']);
+    });
+  }
+
+  updateMedecin() {
+    this.medecinService.updateMedecin(this.id, this.medecin).subscribe(() => {
+      this.router.navigate(['/medecins']);
+    });
+  }
 }
